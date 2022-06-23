f33cb2a8431b1415ac406099ac8851d0fa113ecf
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-06-23 10:23:32 -0700

    Set increment operators to be constexpr

--- src/pos.hpp
+++ src/pos.hpp
@@ -7,14 +7,27 @@ namespace fqs
     struct Pos
     {
         unsigned long m_idx, m_lnum, m_cnum;
-        std::string m_fname, m_fcontents;
+        std::string m_fname, m_ftext;
 
         Pos(const Pos &other) noexcept = default;
         ~Pos() noexcept = default;
 
         Pos &operator=(const Pos &other) noexcept = default;
 
-        void operator+=(char curr) noexcept;
-        void operator++(int) noexcept;
+        constexpr void operator+=(char curr) noexcept
+        {
+            (*this)++;
+
+            if (curr == '\n')
+            {
+                m_lnum++;
+                m_cnum = 0;
+            }
+        }
+        constexpr void operator++(int) noexcept
+        {
+            m_idx++;
+            m_cnum++;
+        }
     };
 }  // namespace fqs
